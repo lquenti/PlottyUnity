85cd1b3bc3e6508912668069ce38f9a11bfc5d5a
Lars Quentin <lars.quentin@stud.uni-goettingen.de>
2022-07-17 15:15:08 +0200

    :construction: optimized square

--- Scripts/MeshPlayground.cs
+++ Scripts/MeshPlayground.cs
@@ -1,30 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
+// TODO: SET PIVOT TO (0,0) for sane meshes
 public class MeshPlayground : Graphic
 {
+    
     protected override void OnPopulateMesh(VertexHelper vh)
     {
         base.OnPopulateMesh(vh);
-        
+        vh.Clear();
         UIVertex v = UIVertex.simpleVert;
         RectTransform rt = GetComponent<RectTransform>();
         Vector2 space = new Vector2(rt.rect.width, rt.rect.height);
 
+        List<Vector2> pts = new List<Vector2>
+        {
+            new Vector2(0f, 0f),
+            new Vector2(space.x, 0f),
+            new Vector2(0, space.y),
+            new Vector2(space.x, space.y)
+        };
 
-
-        v.position = new Vector2(.5f, .5f) * space;
-        vh.AddVert(v);
-        v.position = new Vector2(-.5f,.5f) * space;
-        vh.AddVert(v);
-        v.position = new Vector2(-.5f, -.5f) * space;
-        vh.AddVert(v);
-        v.position = new Vector2(.5f, -.5f) * space;
-        vh.AddVert(v);
-
-        vh.AddTriangle(0, 1, 2);
-        vh.AddTriangle(2, 3, 0);
-    }
+        var verts = pts.Select(v => { var x = UIVertex.simpleVert; x.position = v; return x; }).ToList();
+        vh.AddUIVertexStream(verts, new List<int>(new int[] { 0, 1, 2, 1, 3, 2 }));
+        }
 }
